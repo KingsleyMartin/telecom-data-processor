f0a4d2ee28adf12dd2736b63268345d18bd55224
Kingsley Martin <kingsley.martin@kmstandards.com>
2025-06-17 11:52:47 -0500

    New File

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import CustomerDataProcessor from '@/components/CustomerAddressStandardization';
+import CustomerExtractoir from '@/components/CustomerExtractoir';
 
 export default function Home() {
   return (
     <main className="container mx-auto">
       <div className="space-y-8 py-8">
-        <CustomerDataProcessor />
+        <CustomerExtractoir />
       </div>
     </main>
   );
