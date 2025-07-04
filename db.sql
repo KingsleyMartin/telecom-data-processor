716e068b4926df5c4dcc32285f51b925908c0ae8
diegomurillojsx <diegomurillocorrea@gmail.com>
2025-07-03 23:12:50 -0600

    🚀 Database created for Supabase PostgreSQL

---
+++ db.sql
@@ -0,0 +1,50 @@
+BEGIN;
+
+-- Tabla Partner
+CREATE TABLE IF NOT EXISTS partner (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- Tabla Address
+CREATE TABLE IF NOT EXISTS address (
+  id BIGSERIAL PRIMARY KEY,
+  address1 TEXT NOT NULL,
+  address2 TEXT,
+  city TEXT NOT NULL,
+  state TEXT NOT NULL,
+  zip_code TEXT NOT NULL
+);
+
+-- Tabla Customer
+CREATE TABLE IF NOT EXISTS customer (
+  id BIGSERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  address_id BIGINT NOT NULL
+    REFERENCES address(id)
+    ON DELETE RESTRICT,
+  partner_id BIGINT NOT NULL
+    REFERENCES partner(id)
+    ON DELETE CASCADE
+);
+
+-- Tabla Mapping_Profile
+CREATE TABLE IF NOT EXISTS mapping_profile (
+  id BIGSERIAL PRIMARY KEY,
+  partner_id BIGINT NOT NULL
+    REFERENCES partner(id)
+    ON DELETE CASCADE,
+  name TEXT NOT NULL
+);
+
+-- Tabla Mapping_Field
+CREATE TABLE IF NOT EXISTS mapping_field (
+  id BIGSERIAL PRIMARY KEY,
+  profile_id BIGINT NOT NULL
+    REFERENCES mapping_profile(id)
+    ON DELETE CASCADE,
+  column_name TEXT NOT NULL,
+  normalized_name TEXT NOT NULL
+);
+
+COMMIT;
